bbbc5514a69ce53f24958645358d262ae967637b
Danyal Nemati <Dnemati@csu.fullerton.edu>
2024-05-28 13:36:46 -0700

    first commit

---
+++ GAN.py
@@ -0,0 +1,10 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torchvision.utils import save_image
+import torchvision.datasets as dset
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+import torchvision.utils as vutils
+
+print ("hello1  ")
