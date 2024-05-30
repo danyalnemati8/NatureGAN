64fc1bcf95a3a3290f715c7db0bc9ac67e68c94b
Danyal Nemati <Dnemati@csu.fullerton.edu>
2024-05-30 11:29:49 -0700

    plot label

--- GAN.py
+++ GAN.py
@@ -1,10 +1,17 @@
 import torch
-import torch.nn as nn
-import torch.optim as optim
-from torchvision.utils import save_image
-import torchvision.datasets as dset
-import torchvision.transforms as transforms
-from torch.utils.data import DataLoader
-import torchvision.utils as vutils
+from torch import nn
 
-print ("hello1  ")
+import math
+import matplotlib.pyplot as plt
+
+torch.manual_seed(111)
+
+train_data_length = 1024
+train_data = torch.zeros((train_data_length, 2))
+train_data[:, 0] = 2 * math.pi * torch.rand(train_data_length)
+train_data[:, 1] = torch.sin(train_data[:, 0])
+train_labels = torch.zeros(train_data_length)
+train_set = [
+    (train_data[i], train_labels[i]) for i in range(train_data_length)
+]
+plt.plot(train_data[:, 0], train_data[:, 1], ".")
