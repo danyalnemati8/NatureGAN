e2d3bd601d0517d6f756e9fe3f1727c6f2ef845e
Danyal Nemati <Dnemati@csu.fullerton.edu>
2024-06-04 11:28:23 -0700

    generated samples

--- GAN.py
+++ GAN.py
@@ -106,3 +106,10 @@ for epoch in range(num_epochs):
         if epoch % 10 == 0 and n == batch_size - 1:
             print(f"Epoch: {epoch} Loss D.: {loss_discriminator}")
             print(f"Epoch: {epoch} Loss G.: {loss_generator}")
+
+latent_space_samples = torch.randn(100, 2)
+generated_samples = generator(latent_space_samples)
+
+generated_samples = generated_samples.detach()
+plt.plot(generated_samples[:, 0], generated_samples[:, 1], ".")
+plt.show()
